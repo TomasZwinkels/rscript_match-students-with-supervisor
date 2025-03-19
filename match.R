9e4842cd860cae34b71aca684371ff59825cca8c
TomasZwinkels <github@zwinkels.co.uk>
2025-03-19 09:50:39 +0100

    script now also imports the supervisor assignments from HWS

--- match.R
+++ match.R
@@ -35,7 +35,12 @@
 	QRAW$SNR <-  as.numeric(QRAW$SNR)
 	
 # import the datafile with supervisor info
-	SUIN <- read.xlsx("feb2025_supervisorinfo.xlsx", sheet = 1)
+	SUINRAW <- read.xlsx("feb2025_supervisorinfo.xlsx", sheet = 1)
+	SUINRAW
+	nrow(SUINRAW)
+	
+	SUIN <- SUINRAW[which(SUINRAW$group == "us"),]
+	
 	head(SUIN)
 	nrow(SUIN)
 	
@@ -53,9 +58,11 @@
 
 	# manual deletes go here
 
-	# Reza Barzegar heeft mogelijk geen nieuwe circle nodig - volgens haar als ze haar resit haalt?
+	# Reza Barzegar heeft mogelijk geen nieuwe circle nodig - volgens haar als hij zijn resit haalt? -- Reza heeft besloten aan een nieuwe circle te willen beginnen.
 	# QRAW <- QRAW[!(QRAW$ResponseId == "R_8me08mfUVpjSDKN"),]
 
+	# Yana  Elisabeth van den Berg submitted here preferences twice, we will only use the 2nd submission
+	QRAW <- QRAW[!(QRAW$ResponseId == "R_2ZTWzeJkO8IlQRv"),]
 
 	nrow(QRAW)	
 	
@@ -73,6 +80,9 @@
 		}
 		noduplicates_SNR(QRAW)
 		QRAW[which(duplicated(QRAW$SNR)),]# return them if they are there
+		
+		QRAW[which(QRAW$SNR == "2078052"),]# this student submitted here preferences twice, i'll drop the first occurence
+		
 		
 		QRAW$email <- tolower(QRAW$email)
 		noduplicates_email <- function(DF) {
@@ -122,7 +132,7 @@
 		OTH$full_name
 		OTH$email
 		
-		# this student came up, and mentions that if she pases her MTP she does not need a circle?
+		# this student came up, they want to just start their thesis again (I, Tomas checked by email).
 		QRAW$extended_master[which(QRAW$ResponseId == "R_8me08mfUVpjSDKN")] <- "No, I did NOT apply for the extended master."
 		
 		## if any Other, break stuf!
@@ -180,12 +190,12 @@
 			n_slots_per_supervisor_vec <- rep(4, length.out = nrow(SUIN))
 			length(n_slots_per_supervisor_vec)
 			
-			n_slots_per_supervisor_vec <- c(4,4,4,4,0,0) # manual version, needs to as long as n_supervisors
+			n_slots_per_supervisor_vec <- c(4,4,4,4,4,0) # manual version, needs to as long as n_supervisors
 			length(n_slots_per_supervisor_vec)
 			
 			# some info here on what are (im)polpular supervisors - note: only work after actweights has been generated below (lower values indicated more popular)
 				# note for next year, is one supervisor keeps on getting only few students, the optimal way to 'force' student onto that supervisor is by giving the relatively impopular supervisors less students in the list above.
-			rbind(n_slots_per_supervisor_vec,LETTERS[1:totalnrsupervisors],unname(colSums(actweights)))
+				rbind(n_slots_per_supervisor_vec,LETTERS[1:totalnrsupervisors],unname(colSums(actweights)))
 		
 			# some inspections, do we have enough supervisors?
 				# enough? - should return TRUE
@@ -403,23 +413,6 @@
 		painmatrix
 		sum(painmatrix)
 		
-	# manual assignment of extended master students
-	#	SPEM <- subset(SPEM, select=c("full_name","email","SNR","what_master_track","extended_master","student_explanation"))
-	#	
-	#	names(SPEM)
-	#	SPEM
-	#
-	#	SPEM$my_assigned_supervisor <- NA
-	#	
-	#	SPEM$my_assigned_supervisor[which(SPEM$email == "R.j.barzegar@tilburguniversity.edu")] <- "C"
-	#	SPEM$my_assigned_supervisor[which(SPEM$email == "g.g.greco@tilburguniversity.edu")] <- "E"
-	#	SPEM$my_assigned_supervisor[which(SPEM$email == "r.s.p.hurl@tilburguniversity.edu")] <- "G"
-	#	SPEM$my_assigned_supervisor[which(SPEM$email == "j.m.vdrMeer_1@tilburguniversity.edu")] <- "I"
-	#	SPEM$my_assigned_supervisor[which(SPEM$email == "w.b.p.kwint@tilburguniversity.edu")] <- "B"
-	#	SPEM$my_assigned_supervisor[which(SPEM$email == "t.j.p.vanaert@tilburguniversity.edu")] <- "A"
-	
-	#	SPEM$how_painfull <- NA
-
 	# now let's get a dataframe I can export where the unit of analysis is the student and we add the details of their assigned supervisor
 		head(SPRF)
 		names(SPRF)
@@ -516,8 +509,40 @@
 		
 		# here the result file from Melanie can be added!
 		
+			# first, lets export the format as a template she can fill in
 		
+				# Get the current date and time
+				current_time <- Sys.time()
+					
+				# Format it into a string suitable for a file name
+				time_str <- format(current_time, "%Y%m%d_%H%M%S")
+
+				# Create a file name with the timestamp
+				file_name <- paste0("template_HWS_supervisors", time_str, ".xlsx")
+			#	write.xlsx(EXPO, file_name)
+			
+			head(EXPO)
+		
+		# load this file again, clean it up a bit and append it
+		HWSSUP <- read.xlsx("HWS_supervisors20250318.xlsx", sheet = "focused")
 	
+		HWS_EXPO <- HWSSUP[,c("full_name", "email", "SNR", "extended_master", "double_degree","last_name_of_assigned_supervisor")]
+		names(HWS_EXPO)[names(HWS_EXPO) == "last_name_of_assigned_supervisor"] <- "my_assigned_supervisor"
+
+		HWS_EXPO$what_master_track <- "HWS"
+		HWS_EXPO$double_degree[is.na(HWS_EXPO$double_degree)] <- "No, I am not following the double degree program."
+
+		HWS_EXPO
+		
+		# Convert SNR to character in both data frames
+			EXPO <- EXPO %>% mutate(SNR = as.character(SNR))
+			HWS_EXPO <- HWS_EXPO %>% mutate(SNR = as.character(SNR))
+
+			# Now row-bind them
+			EXPO <- bind_rows(EXPO, HWS_EXPO)
+			
+			EXPO
+					
 # now randomly assign the 2nd reader
 		
 		# lets set the seed using the cohort as the input, so that the random selection is the same when we run the script, but different between cohorts.
@@ -530,10 +555,20 @@
 			# get a vector with all circles that need to be assigned
 				circles_that_still_need_a_2nd_reader <- unique(EXPO$my_assigned_supervisor)
 				
-			# OPTIONAL - also add the last names of the supervisors from HWS here (as 'letters') if we also want them to be assigned a supervisor
+			# OPTIONAL - also add the last names of the supervisors from HWS here (as 'letters') if we also want them to be assigned a supervisor (did this now, but not as letters)
 			
-			# Randomly shuffle this vector
-				circles_that_still_need_a_2nd_reader_rand <- circles_that_still_need_a_2nd_reader[sample(length(circles_that_still_need_a_2nd_reader))]
+			# Randomly shuffle this vector per group
+			
+				# get a vector per group
+					circles_that_still_need_a_2nd_reader_forus <- circles_that_still_need_a_2nd_reader[which(nchar(circles_that_still_need_a_2nd_reader) == 1)]
+					circles_that_still_need_a_2nd_reader_forHWS <- circles_that_still_need_a_2nd_reader[which(nchar(circles_that_still_need_a_2nd_reader) > 1)]
+
+				# reshuffle each vector
+					circles_that_still_need_a_2nd_reader_forus_rand <- circles_that_still_need_a_2nd_reader_forus[sample(length(circles_that_still_need_a_2nd_reader_forus))]
+					circles_that_still_need_a_2nd_reader_forHWS_rand <- circles_that_still_need_a_2nd_reader_forHWS[sample(length(circles_that_still_need_a_2nd_reader_forHWS))]
+				# bind
+					circles_that_still_need_a_2nd_reader_rand <- c(circles_that_still_need_a_2nd_reader_forus_rand,circles_that_still_need_a_2nd_reader_forHWS_rand)
+				# circles_that_still_need_a_2nd_reader_rand <- circles_that_still_need_a_2nd_reader[sample(length(circles_that_still_need_a_2nd_reader))]
 			
 			# loop through each circle of the random list, and assign the person that currently has the lowest number of 2nd reader tasks, until the list is empty.
 
@@ -575,14 +610,42 @@
 				EXPO
 
 			# now, remove this assignment for students that are doing a double degree
-			EXPO$random2ndreader_ANR[which(grepl("Yes, I am doing the double-degree",EXPO$double_degree, fixed=TRUE))] <- NA
-			EXPO$random2ndreader_last_name[which(grepl("Yes, I am doing the double-degree",EXPO$double_degree, fixed=TRUE))] <- NA
+			EXPO$random2ndreader_ANR[which(grepl("Yes, I am doing the double-degree",EXPO$double_degree, fixed=TRUE))] <- "Double Degree student, assign manually!"
+			EXPO$random2ndreader_last_name[which(grepl("Yes, I am doing the double-degree",EXPO$double_degree, fixed=TRUE))] <- "Double Degree student, assign manually!"
 			EXPO$random2ndreader_first_name[which(grepl("Yes, I am doing the double-degree",EXPO$double_degree, fixed=TRUE))] <- NA
 			EXPO$random2ndreader_email[which(grepl("Yes, I am doing the double-degree",EXPO$double_degree, fixed=TRUE))] <- NA
 			
 			EXPO
 	
-# EXPORT THE COMPLETE FILE
+	# merge in more information on the supervisors
+				nrow(EXPO)
+				EXPO <- sqldf("SELECT EXPO.*, SUINRAW.last_name as 'supervisor_last_name', SUINRAW.first_name as 'supervisor_first_name', SUINRAW.email as 'supervisor_email'
+							FROM EXPO
+							LEFT JOIN SUINRAW
+							ON EXPO.my_assigned_supervisor = SUINRAW.letter_in_feb2025surv")
+				nrow(EXPO)
+				EXPO	
+				
+	# do some check sums on the circle sizes
+	
+		table(HWSSUP$last_name_of_assigned_supervisor)
+		table(EXPO$supervisor_last_name)
+		
+		# Create the two tables
+			tbl1 <- table(HWSSUP$last_name_of_assigned_supervisor)
+			tbl2 <- table(EXPO$supervisor_last_name)
+
+		# Subset tbl2 to only include names that appear in tbl1,
+			# keeping the same order as in tbl1
+			tbl2_subset <- tbl2[names(tbl1)]
+
+		# Optionally, replace any NA (if a name in tbl1 is missing from tbl2) with 0
+			tbl2_subset[is.na(tbl2_subset)] <- 0
+
+		# Combine the tables by rows
+			rbind(tbl1, tbl2_subset)
+	
+# EXPORT THE COMPLETE FILE with details
 
 	# Get the current date and time
 	current_time <- Sys.time()
@@ -591,9 +654,32 @@
 	time_str <- format(current_time, "%Y%m%d_%H%M%S")
 
 	# Create a file name with the timestamp
-	file_name <- paste0("suggested_student-to-supervisor_assignments_", time_str, ".xlsx")
+	file_name <- paste0("details_suggested_student-to-supervisor_assignments_", time_str, ".xlsx")
 	write.xlsx(EXPO, file_name)
 				
+
+# EXPORT THE OVERVIEW AS WE USE IT IN THE GOOGLE sheet
+
+	# course codes 
+		# SET the course codes
+		PPSD_coursecode <- "441803-M-24"
+		PPSinCP_coursecode <- "400991-M-24"
+		HWS_coursecode <- "400913-M-24"
+	
+		EXPO$track_code <- NA
+		EXPO$track_code <- ifelse(grepl("GSMI", EXPO$what_master_track), PPSD_coursecode, NA)
+		EXPO$track_code <- ifelse(grepl("PPSinCP", EXPO$what_master_track), PPSinCP_coursecode, EXPO$track_code)
+		table(EXPO$track_code)
+		nrow(EXPO) == sum(table(EXPO$track_code)) # should return TRUE
+
+
+	EXPO2 <- EXPO[,c("SNR","track_code","full_name","email","supervisor_last_name","supervisor_first_name","supervisor_email","random2ndreader_last_name","random2ndreader_first_name","random2ndreader_email"),]
+
+	# Create a file name with the timestamp
+	file_name <- paste0("more_focussed_suggested_student-to-supervisor_assignments_", time_str, ".xlsx")
+	write.xlsx(EXPO2, file_name)
+
+
 			# PLEASE NOTE: THIS PART ALWAYS NEEDS TO BE RUN MANUALLY 
 			#
 			#	OPTION 1: SELECT DATA FROM ABOVE
